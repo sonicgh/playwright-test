ab588b728bfb31e9cb49c1b0dff68e46ace7d073
Jose <josedex1@hotmail.com>
2024-07-08 23:33:51 -0500

    fixing a spelling error

--- test/test_codegen.py
+++ test/test_codegen.py
@@ -1,4 +1,4 @@
-v#type in console playwright codegen or playwright codegen XX where XX=URL
+#type in console playwright codegen or playwright codegen XX where XX=URL
 import pytest
 from playwright.sync_api import Page, expect
 
